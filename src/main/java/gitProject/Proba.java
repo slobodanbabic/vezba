ba70e7c011cfc8e3e4bae6d37f9b33709e0b15f4
slobodanbabic <slobodannbabic@gmail.com>
2020-10-15 23:33:55 +0200

    Initial commit

--- src/main/java/gitProject/Proba.java
+++ src/main/java/gitProject/Proba.java
@@ -5,15 +5,19 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.Writer;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.TextProgressMonitor;
 
 public class Proba {
 
 	public static void main(String[] args) throws Exception {
-
+		// Monitor to get git command progress printed on java System.out console
+		TextProgressMonitor consoleProgressMonitor = new TextProgressMonitor(new PrintWriter(System.out));
 		Writer writer = null;
 		File fajl = new File("NovaKlasa.java");
 
@@ -51,6 +55,9 @@ public class Proba {
 
 		git.commit().setMessage("Initial commit").call();
 		System.out.println("Committed file " + fajl + " to repository at " + git.getRepository().getDirectory());
+		System.out.println("\n>>> Printing status of local repository\n");
+		Status status = git.status().setProgressMonitor(consoleProgressMonitor).call();
+		System.out.println("Modified file = " + status.getModified());
 
 	}
 }
